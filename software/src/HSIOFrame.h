e66c6e0b27ff273067f4cc9166c776583d7d2588
Beau Sterling <beau.seidon@outlook.com>
2025-06-16 21:39:16 -0500

    fixed building for T40, moved vid/pid list to library

--- software/src/HSIOFrame.h
+++ software/src/HSIOFrame.h
@@ -18,7 +18,7 @@
 #include "HSicons.h"
 #include "HSClockManager.h"
 
-#ifdef ARDUINO_TEENSY41
+#ifdef __IMXRT1062__
 #include "HSGamepad.h"
 #endif
 
@@ -490,7 +490,7 @@ struct MIDIFrame {
     }
 };
 
-#ifdef ARDUINO_TEENSY41
+#ifdef __IMXRT1062__
 struct GamepadMapping {
     // static constexpr size_t Size = 32; // Make this compatible with Packable
     // uint32_t Pack() const {
@@ -562,7 +562,7 @@ struct IOFrame {
     /* MIDI message queue/cache */
     MIDIFrame MIDIState;
 
-#ifdef ARDUINO_TEENSY41
+#ifdef __IMXRT1062__
     GamepadFrame GamepadState;
 #endif
 
