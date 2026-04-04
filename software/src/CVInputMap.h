7b7670077a9418fc7a0cb7420c7c3b651ced4107
Beau Sterling <beau.seidon@outlook.com>
2026-04-04 01:11:04 -0500

    CVInputMap ViewOut fixup. still too big to compile properly

--- software/src/CVInputMap.h
+++ software/src/CVInputMap.h
@@ -30,7 +30,7 @@ struct CVInputMap {
     return (source <= ADC_CHANNEL_LAST)
       ? frame.inputs[source - 1]
       : (source - ADC_CHANNEL_LAST <= DAC_CHANNEL_LAST)
-        ? frame.outputs[source - 1 - ADC_CHANNEL_LAST]
+        ? frame.ViewOut(source - 1 - ADC_CHANNEL_LAST)
 #ifdef ARDUINO_TEENSY41
         : (source - ADC_CHANNEL_LAST - DAC_CHANNEL_LAST <= MIDIMAP_MAX )
             ? frame.MIDIState.mapping[source - ADC_CHANNEL_LAST - DAC_CHANNEL_LAST - 1 ].output
